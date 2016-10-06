07dc919144a96de7f65f980f53b1f3ab160e8eeb
Albert Ramos <albertramos@protonmail.com>
2016-10-06 15:33:22 +0200

    add url method to postcollection

--- src/app/Collections/Post/PostsCollection.js
+++ src/app/Collections/Post/PostsCollection.js
@@ -1,11 +1,11 @@
 define(function (require) {
-
     var Backbone = require('backbone');
     var Post = require('app/Models/Post/Post');
+    var Config = require('config');
 
     var Posts = Backbone.Collection.extend({
         model: Post,
-        url: 'http://api.blogjs.io/posts'
+        url: Config.getApiUrl('/posts')
     });
 
     return Posts;
