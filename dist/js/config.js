b4d88b9c3178b06741c7438f9082d73c3b88ecc4
Seyn <seyn@outlook.com>
2016-10-07 22:23:44 +0200

    Domain change

--- dist/js/config.js
+++ dist/js/config.js
@@ -1,7 +1,7 @@
 define(function (require) {
 
     var Config = {
-      base_url: 'http://api.blogjs.io',
+      base_url: 'http://api.blog.albertramos.me',
 
       getApiUrl: function(url){
         return this.base_url + url;
